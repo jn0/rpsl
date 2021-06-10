eece891edd9f5a6e4f928e0c991ba30d93231bf4
jno <jno@tcinet.ru>
2021-06-10 12:07:45 +0300

    improve configuration algo; provide for logging config

--- rpsl/__main__.py
+++ rpsl/__main__.py
@@ -1,18 +1,29 @@
 #!python3
 '''
 '''
-import logging; log = logging.getLogger(__name__)  # noqa E702
+import logging; log = logging.getLogger(__package__)  # noqa E702
+import logging.config
 import sys
 import os
 from .parser import parse
+from .config import config
 
 logging.basicConfig(level=logging.INFO)
+cf = config()
+if 'logging' in cf:
+    lcf = cf.pop('logging')
+    if lcf:
+        logging.config.dictConfig(lcf)
 
-for arg in sys.argv[1:]:
-    if os.path.exists(arg):
-        with open(arg) as f:
-            for o in parse(f, exceptions=False):
-                print(o, end='\n' + '~' * 80 + '\n')
-    else:
-        log.warning('No file %r', arg)
+log.info('begin')
+try:
+    for arg in sys.argv[1:]:
+        if os.path.exists(arg):
+            with open(arg) as f:
+                for o in parse(f, exceptions=False):
+                    print(o, end='\n' + '~' * 80 + '\n')
+        else:
+            log.warning('No file %r', arg)
+finally:
+    log.info('end')
 # vim:set ft=python ai et ts=4 sts=4 sw=4 cc=80:EOF #
--- rpsl/config.py
+++ rpsl/config.py
@@ -7,17 +7,48 @@ import yaml
 
 VERSION = 0
 _config = {}
+_loaded = set()
+
+
+def load(filename):
+    if filename in _loaded:
+        log.warning('Config file %r is already loaded', filename)
+        return {}
+    _loaded.add(filename)
+    with open(filename) as f:
+        c = yaml.load(f, yaml.SafeLoader)
+        assert c.get('version', -1) == VERSION, \
+               f"config {filename!r}" \
+               f" version {c.get('version')!r} != {VERSION!r}"
+        if 'include' in c:
+            dirname = os.path.dirname(filename)
+            for name in c.pop('include', []):
+                i = load(name if os.path.isabs(name)
+                         else os.path.join(dirname, name))
+                c.update(i)
+        log.info('Config file %r has been loaded', filename)
+        return c
+
+
+def locate(name=None, suffix='.yaml'):
+    filename = os.path.realpath(os.path.splitext(name or __file__)[0] + suffix)
+    while True:
+        if os.path.exists(filename):
+            return filename
+        d, n = os.path.split(filename)
+        if d == '/' or not d:
+            return None
+        filename = os.path.join(os.path.dirname(d), n)
 
 
 def config():
     if not _config:
-        filename = os.path.splitext(__file__)[0] + '.yaml'
-        with open(filename) as f:
-            c = yaml.load(f, yaml.SafeLoader)
-            assert c.get('version', -1) == VERSION, \
-                   f"config version {c.get('version')!r} != {VERSION!r}"
-        _config.update(c)
-        log.info('Config file %r has been loaded', filename)
+        filename = locate()
+        if not filename:
+            log.fatal('No config.')
+            return {}  # not the _config object!
+        r = load(filename)
+        _config.update(r)
     return _config
 
 
